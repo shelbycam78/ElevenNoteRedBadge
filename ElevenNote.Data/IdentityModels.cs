12769a95f3f864f2f11290702bc50db6b474953f
Shelby Simpson <shelbycam78@gmail.com>
2021-10-14 18:53:49 -0400

    Identity Models Setup

--- ElevenNote.Data/IdentityModels.cs
+++ ElevenNote.Data/IdentityModels.cs
@@ -1,4 +1,6 @@
 ﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -29,5 +31,35 @@ namespace ElevenNote.Data
         {
             return new ApplicationDbContext();
         }
+        public DbSet<Note> Notes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Conventions
+                .Remove<PluralizingTableNameConvention>();
+
+            modelBuilder
+                .Configurations
+                .Add(new IdentityUserLoginConfiguration())
+                .Add(new IdentityUserRoleconfiguration());
+        }
+        public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+        {
+            public IdentityUserLoginConfiguration()
+            {
+                HasKey(iul => iul.UserId);
+            }
+        }
+
+        public class IdentityUserRoleconfiguration : EntityTypeConfiguration<IdentityUserRole>
+        {
+            public IdentityUserRoleconfiguration()
+            {
+                HasKey(iur => iur.UserId);
+            }
+        }
     }
+
+
 }
